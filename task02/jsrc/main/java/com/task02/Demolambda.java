15c77714765d787833a3829a5985e68994112532
yura <>
2024-01-22 19:03:24 +0200

    task02

--- task02/jsrc/main/java/com/task02/Demolambda.java
+++ task02/jsrc/main/java/com/task02/Demolambda.java
@@ -9,6 +9,10 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "DemoLambda-role",
 	isPublishVersion = true
@@ -18,27 +22,33 @@ import java.util.Map;
 authType = AuthType.NONE,
 invokeMode = InvokeMode.BUFFERED
 )
-public class Demolambda implements RequestHandler<Object, Map<String, Object>> {
+public class Demolambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	private final Gson gson = new Gson();
+
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 
 
-		resultMap.put("statusCode", "200");
+	//	resultMap.put("statusCode", "200");
 		Body body = new Body();
-		body.statusCode = "200";
+		body.statusCode = 200;
 		body.message = "Hello from Lambda";
 
-		resultMap.put("body", body);
-	//	resultMap.put("statusCode","200");
-	//	resultMap.put("message","Hello from Lambda");
-		return resultMap;
+	//	resultMap.put("body", body);
+	////	resultMap.put("statusCode","200");
+	////	resultMap.put("message","Hello from Lambda");
+	//	return resultMap;
+
+	return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody(gson.toJson(body));
 	}
 }
 
 class Body {
-	public String statusCode;
+	public int statusCode;
 	public String message;
 }
 
