c4f12242d3f7278f8186178cd4954ac4493d58fa
yura <>
2024-01-22 11:50:51 +0200

    initial commit

---
+++ Task01/jsrc/main/java/com/Task01/Demolambda.java
@@ -0,0 +1,24 @@
+package com.Task01;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "DemoLambda",
+	roleName = "DemoLambda-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}"
+)
+public class Demolambda implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "Hello from Lambda");
+		return resultMap;
+	}
+}
